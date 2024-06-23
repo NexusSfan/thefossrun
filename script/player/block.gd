636bf1a3fd3929b51f93638a4ce7ab0926b95b8f
NexusSfan <nexussfan@duck.com>
2024-06-22 20:22:42 -0700

    foss is BEST AGPL AGPL AGPL

--- script/player/block.gd
+++ script/player/block.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = get_node("../player").playerpos
+	position = Vector2(get_node("../player").playerpos.x, get_node("../player").playerpos.y)
+
+func _on_area_entered(area):
+	print(area.name)
+
+func _on_body_entered(body):
+	print(body.name)
